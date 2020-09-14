37d8423ba9725da5379d1159faf48b0eeb04edc8
Azimcharoliya <39900677+Azimcharoliya@users.noreply.github.com>
2020-09-14 13:48:14 +0530

    Updates Test Cases

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,27 +1,16 @@
 using System;
 using Xunit;
 using Statistics;
+using System.Collections.Generic;
 
 namespace Statistics.Test
 {
     public class StatsUnitTest
     {
         [Fact]
-        public void ReportsAverageMinMaxForFloatValues()
+        public void WhenCalledWithNonEmptyListofNumbersThenReportsAverageMinMaxofNumbers()
         {
-            var statsComputer = new StatsComputer<float>();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<float>{1.5F, 8.9F, 3.2F, 4.5F});
-            float epsilon = 0.001F;
-            Assert.True(Math.Abs(computedStats.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(computedStats.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(computedStats.min - 1.5) <= epsilon);
-        }
-        
-        [Fact]
-        public void ReportsAverageMinMaxForDoubleValues()
-        {
-            var statsComputer = new StatsComputer<double>();
+            var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
                 new List<double>{1.5, 8.9, 3.2, 4.5});
             float epsilon = 0.001F;
@@ -31,29 +20,14 @@ namespace Statistics.Test
         }
         
         [Fact]
-        public void ReportsAverageMinMaxForIntegerValues()
-        {
-            var statsComputer = new StatsComputer<int>();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<int>{1, 2, 3, 4, 5});
-            float epsilon = 0.001F;
-            Assert.True(Math.Abs(computedStats.average - 3) <= epsilon);
-            Assert.True(Math.Abs(computedStats.max - 5) <= epsilon);
-            Assert.True(Math.Abs(computedStats.min - 1) <= epsilon);
-        }
-        
-        [Fact]
-        public void ReportsNaNForEmptyInput()
+        public void WnenCalledWithEmptyListThenReportsNANForAverageMinMax()
         {
             var statsComputer = new StatsComputer<double>();
             var computedStats = statsComputer.CalculateStatistics(
                 new List<double>{});
             Assert.True(Double.IsNaN(computedStats.average));
-            Assert.True(Double.IsNaN(computedStats.max - 8.9));
-            Assert.True(Double.IsNaN(computedStats.min - 1.5));    
-            //All fields of computedStats (average, max, min) must be
-            //Double.NaN (not-a-number), as described in
-            //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
+            Assert.True(Double.IsNaN(computedStats.max));
+            Assert.True(Double.IsNaN(computedStats.min));    
         }
     }
 }
